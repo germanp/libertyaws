2c4ba45b307310a82286eb7aca9d0aba9182de57
federico.cristina@disytel.net <federico.cristina@disytel.net@31491f07-5802-498e-be4e-c1ba56733aaf>
2013-10-03 19:47:17 +0000

    Nuevo servicio generico (cont)

--- trunk/src/org/libertya/ws/handler/CustomServiceHandler.java
+++ trunk/src/org/libertya/ws/handler/CustomServiceHandler.java
@@ -7,6 +7,7 @@ import org.libertya.ws.bean.parameter.CustomServiceParameterBean;
 import org.libertya.ws.bean.result.CustomServiceResultBean;
 import org.libertya.ws.exception.ModelException;
 import org.openXpertya.plugin.common.CustomServiceInterface;
+import org.openXpertya.plugin.common.DynamicArgument;
 import org.openXpertya.plugin.common.DynamicResult;
 import org.openXpertya.util.Env;
 
@@ -23,14 +24,18 @@ public class CustomServiceHandler extends GeneralHandler {
 			CustomServiceInterface instance = (CustomServiceInterface)clazz.newInstance();
 			
 			// parametros para la invocacion (Argumentos, contexto, transaccion)
-			Method method = CustomServiceInterface.class.getMethod(data.getMethodName(), CustomServiceParameterBean.class, Properties.class, String.class);
-			DynamicResult result = (DynamicResult)method.invoke(instance, data, Env.getCtx(), getTrxName());
+			Method method = CustomServiceInterface.class.getMethod(data.getMethodName(), DynamicArgument.class, Properties.class, String.class);
+			DynamicArgument arguments = new DynamicArgument();
+			arguments.setContent(data.arguments);
+			DynamicResult result = (DynamicResult)method.invoke(instance, arguments, Env.getCtx(), getTrxName());
 			
 			// instanciar el objeto a fin de iniciar el procesamiento 
 			CustomServiceResultBean response = new CustomServiceResultBean();
 			
 			// setear valores de respuesta
-			response.setResult(result);
+			response.setError(result.isError());
+			response.setErrorMsg(result.getErrorMsg());
+			response.setResult(result.getContent());
 			
 			// devolver respuesta
 			return response;

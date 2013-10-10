dd3d24957f2d5ae2d3d9223b2a70e9230011b947
federico.cristina@disytel.net <federico.cristina@disytel.net@31491f07-5802-498e-be4e-c1ba56733aaf>
2013-10-10 12:54:11 +0000

    LYWSE: Extensión de LYWS.  Requiere r630 de LYCORE.  En conjunto con r9.

---
+++ trunk/src/org/libertya/wse/result/Result.java
@@ -0,0 +1,34 @@
+package org.libertya.wse.result;
+
+import org.libertya.wse.common.ListedMap;
+
+public class Result {
+
+	/** El resultado fue un error */
+	protected boolean error = false;
+	/** Mensaje de error */
+	protected String errorMsg = "";
+	/** Valores resultantes */
+	protected ListedMap[] resultValues;
+	
+	public boolean isError() {
+		return error;
+	}
+	public void setError(boolean error) {
+		this.error = error;
+	}
+	public String getErrorMsg() {
+		return errorMsg;
+	}
+	public void setErrorMsg(String errorMsg) {
+		this.errorMsg = errorMsg;
+	}
+	public ListedMap[] getResultValues() {
+		return resultValues;
+	}
+	public void setResultValues(ListedMap[] resultValues) {
+		this.resultValues = resultValues;
+	}
+	
+	
+}
